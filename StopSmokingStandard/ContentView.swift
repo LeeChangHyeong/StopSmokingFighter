34d016d30f53d4e22694ca6a162c483acae037a4
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-05-05 18:14:00 +0900

    Initial Commit

---
+++ StopSmokingStandard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StopSmokingStandard
+//
+//  Created by 이창형 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StopSmokingStandard/StopSmokingStandardApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopSmokingStandardApp.swift
+//  StopSmokingStandard
+//
+//  Created by 이창형 on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct StopSmokingStandardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
